0993e214a9c61e3dcdcfe437e2676e0de40f11c7
mugimaru <pochi.73@gmail.com>
2021-12-08 21:39:52 +0400

    y2021 d05 p2

--- 2021/day05/solution.py
+++ 2021/day05/solution.py
@@ -2,25 +2,30 @@ from collections import defaultdict
 
 
 def part1(input: str) -> int:
-    acc = defaultdict(int)
-
-    for (x1, y1), (x2, y2) in _parse_input(input):
-        if x1 == x2:
-            for y in range(min(y1, y2), max(y1, y2) + 1):
-                acc[(x1, y)] += 1
-        if y1 == y2:
-            for x in range(min(x1, x2), max(x1, x2) + 1):
-                acc[(x, y1)] += 1
-
-    intersections = 0
-    for (x, y), count in acc.items():
-        if count > 1:
-            intersections += 1
-    return intersections
+    return _calculate_intersections(_parse_input(input), with_diagonal_lines=False)
 
 
 def part2(input: str) -> None:
-    return None
+    return _calculate_intersections(_parse_input(input), with_diagonal_lines=True)
+
+def _calculate_intersections(input: list[tuple[tuple[int, int], tuple[int, int]]], with_diagonal_lines=False) -> int:
+    acc = defaultdict(int)
+
+    for (x1, y1), (x2, y2) in input:
+        if with_diagonal_lines or x1 == x2 or y1 == y2:
+            while x1 != x2 or y1 != y2:
+                acc[(x1, y1)] += 1
+                if x1 != x2:
+                    x1 += 1 if x1 < x2 else -1
+                if y1 != y2:
+                    y1 += 1 if y1 < y2 else -1
+            acc[(x1, y1)] += 1
+
+    intersections = 0
+    for _, count in acc.items():
+        if count > 1: intersections += 1
+
+    return intersections
 
 
 def _parse_input(input: str) -> list[tuple[tuple[int, int], tuple[int, int]]]:
